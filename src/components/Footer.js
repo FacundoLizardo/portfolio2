5eca895676404996b705f9998816476178c1ede0
FacundoLizardo <facundolizardo75@gmail.com>
2024-07-09 16:03:13 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,68 +8,60 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 import {CgGitFork} from "react-icons/cg";
 import Button from "react-bootstrap/Button";
+import {useLocation} from "react-router-dom";
+
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+  const location = useLocation()
+  console.log(location)
+
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Facundo Lizardo</h3>
+          <h3></h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} FL</h3>
+          <h3>Designed and developed by Facundo Lizardo</h3>
         </Col>
-        <Col md="4" className="footer-body">
+        {location.pathname !== '/' &&
+            <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/FacundoLizardo"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
+                  href="https://github.com/FacundoLizardo"
+                  style={{color: "white"}}
+                  target="_blank"
+                  rel="noopener noreferrer"
               >
-                <AiFillGithub />
+                <AiFillGithub/>
               </a>
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/facundolizardo/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/facundolizardo/"
+                  style={{color: "white"}}
+                  target="_blank"
+                  rel="noopener noreferrer"
               >
-                <FaLinkedinIn />
+                <FaLinkedinIn/>
               </a>
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/faculizardo640/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
+                  href="https://www.instagram.com/faculizardo640/"
+                  style={{color: "white"}}
+                  target="_blank"
+                  rel="noopener noreferrer"
               >
-                <AiFillInstagram />
+                <AiFillInstagram/>
               </a>
             </li>
 
-            <li>
-              <div className="fork-btn">
-                <Button
-                  href="https://github.com/FacundoLizardo/Portfolio2"
-                  target="_blank"
-                  className="fork-btn-inner"
-                >
-                   <div style={{display: 'flex', flexDirection: 'row', gap: '5px', fontSize: '1.2em'}}>
-                        <CgGitFork style={{fontSize: "1.2em"}}/>
-                        <p className="tech-icons-text" style={{fontSize: '2em'}}>This repository</p>
-                  </div>
-                </Button>
-              </div>
-            </li>
           </ul>
-        </Col>
+        </Col>}
       </Row>
     </Container>
   );
