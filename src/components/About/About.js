4d2654628f11f50efafd8457b6f1397d4fd57e2f
FacundoLizardo <facundolizardo75@gmail.com>
2024-07-10 21:58:09 -0300

    profile pic styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,24 +13,25 @@ function About() {
       <Container>
         <Row style={{ justifyContent: "center", padding: "10px" }}>
           <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
+              md={7}
+              style={{
+                justifyContent: "center",
+                paddingTop: "30px",
+                paddingBottom: "50px",
+              }}
           >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+            <img src={profilepic} alt="about" className="profilepic-above"/>
+            <h1 style={{fontSize: "2.1em", paddingBottom: "20px"}}>
               Know Who <strong className="purple">I'M</strong>
             </h1>
-            <Aboutcard />
+            <Aboutcard/>
           </Col>
           <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+              md={5}
+              style={{paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
           >
-            <img src={profilepic} alt="about" className="profilepic" />
+            <img src={profilepic} alt="about" className="profilepic-side" />
           </Col>
         </Row>
         <h1 className="project-heading">
