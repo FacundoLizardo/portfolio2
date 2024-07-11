53e5b3985b7b686140ded38511da28a230f9b308
FacundoLizardo <facundolizardo75@gmail.com>
2024-07-10 21:01:14 -0300

    styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,29 +13,34 @@ function Home() {
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
+              <div style={{display:'flex', flexDirection:'column', margin:'auto'}}>
+                <h1 style={{paddingBottom: 15, width:'100%'}} className="heading">
+                  Hi There!{" "}
+                  <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
-              </h1>
+                </h1>
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> FACUNDO LIZARDO</strong>
-              </h1>
+                <h1 className="heading-name">
+                  I'M
+                  <strong className="main-name"> FACUNDO LIZARDO</strong>
+                </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
+                <div style={{padding: 50, textAlign: "left"}}>
+                  <Type/>
+                </div>
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className={'main-image-avatar1'}
-              />
+            <Col md={5} style={{ paddingBottom:20}}>
+
+              <div style={{display:'flex', margin:'auto'}}>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className={'main-image-avatar1'}
+                />
+              </div>
             </Col>
           </Row>
         </Container>
