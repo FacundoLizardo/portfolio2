ff2b6e347f3f600aea2885ec74ae6041de10d3b4
FacundoLizardo <facundolizardo75@gmail.com>
2024-07-10 22:24:08 -0300

    profile pic styles

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -7,51 +7,51 @@ import {FaNodeJs} from "react-icons/fa";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
         <DiCss3/>
           <p className="tech-icons-text">CSS</p>
       </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <DiJavascript1/>
             <p className="tech-icons-text">JavaScript</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiTypescript/>
             <p className="tech-icons-text">TypeScript</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <FaNodeJs/>
             <p className="tech-icons-text">Node.js</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <DiReact/>
             <p className="tech-icons-text">React.js</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <DiHtml5/>
             <p className="tech-icons-text">HTML</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiPostgresql/>
             <p className="tech-icons-text">PostgreSQL</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiNextdotjs/>
             <p className="tech-icons-text">Next.js</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <DiGit/>
             <p className="tech-icons-text">GIT</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiTailwindcss/>
             <p className="tech-icons-text">Tailwind</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiRedux/>
             <p className="tech-icons-text">Redux</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiExpress/>
             <p className="tech-icons-text">Experss.js</p>
         </Col>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,31 +6,31 @@ import {BsGithub, BsTrello} from "react-icons/bs";
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiWindows/>
             <p className="tech-icons-text">Windows</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiVisualstudiocode/>
             <p className="tech-icons-text">VS Code</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiIntellijidea/>
             <p className="tech-icons-text">Intellij IDEA</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiSlack/>
             <p className="tech-icons-text">Slack</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <SiDiscord/>
             <p className="tech-icons-text">Discord</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <BsGithub/>
             <p className="tech-icons-text">GitHub</p>
         </Col>
-        <Col xs={4} md={2} className="tech-icons">
+        <Col xs={8} sm={4} md={4} lg={4} xl={2} className="tech-icons">
             <BsTrello/>
             <p className="tech-icons-text">Trello</p>
         </Col>
